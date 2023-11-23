e31fe3ab4beb10b04a4bbe1a8322b2985962cde3
Adiladis <adam.lysen@elev.ga.ntig.se>
2023-11-23 11:01:03 +0100

    dfgdfgdfg

--- Vinterprojekt/Program.cs
+++ Vinterprojekt/Program.cs
@@ -1,10 +1,44 @@
 ﻿using System;
+using System.ComponentModel;
 
-class Program {
+class Program
+{
 
-    static void Main(){
+    
 
+    static void Main()
+    {
         
+        Console.WriteLine("Welcome to the Driving license simulator 2024!");
+        Console.WriteLine();
+        Console.WriteLine("Press enter to play");
+        Console.WriteLine("Write ESC to exit");
+        string begin = Console.ReadLine();
+
+
+        if (begin == "ESC" || begin == "esc") {
+            return;
+        }
+        
+
+        Console.WriteLine("You are about to get 3 questions");
+        Console.WriteLine("Get them all right to advance to the driving test");
+        Console.WriteLine();
+        Console.WriteLine("Press enter to start the test!");
+        Console.ReadLine();
+        Console.WriteLine("Starting test...");
+        Thread.Sleep(3000);
+        Console.WriteLine("3");
+        Thread.Sleep(1000);
+        Console.WriteLine("2");
+        Thread.Sleep(1000);
+        Console.WriteLine("1");
+        Thread.Sleep(1000);
+        Console.WriteLine("First question:");
+        Console.WriteLine("difrifrifjifidfk");
+        string answer1 = Console.ReadLine();
+
+
 
 
     }
